f997d39e6bf2676fcdc24e869578e0ad7792d9be
DanielKulaga <danielkulaga@outlook.com>
2022-08-30 20:37:36 +0200

    Delete backend directory

--- backend/api/v1/routes/contactsRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const auctionsController = require("../controllers/contactsController")
-
-router.get('/', contactsController.getAllContacts);
-
-
-module.exports = router;
